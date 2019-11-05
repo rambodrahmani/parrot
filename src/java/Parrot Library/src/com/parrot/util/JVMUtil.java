e6b0fc2f176b27553d79be01427b6804a16e0bbd
Rambod Rahmani <rambodrahmani@autistici.org>
2019-11-05 23:02:07 +0100

    Add new utility method to check for root privileges.

--- src/java/Parrot Library/src/com/parrot/util/JVMUtil.java
+++ src/java/Parrot Library/src/com/parrot/util/JVMUtil.java
@@ -1,5 +1,10 @@
 package com.parrot.util;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.prefs.Preferences;
+
 /**
  * JVM Related Utilities Class.
  *
@@ -28,7 +33,7 @@ public final class JVMUtil
     {
         return System.getProperty("sun.arch.data.model");
     }
-	
+
     /**
      * @return  true if the running JVM is 64-bit, false otherwise: either the
      *          running JVM has 32-bit or unknown architecture.
@@ -37,7 +42,59 @@ public final class JVMUtil
     {
         return getJVMArch().indexOf("64") != -1;
     }
-	
+
+    /**
+     * Checks if the application is running with root/admin privileges or not.
+     * This solution is platform-independent: it tries to write system
+     * preferences and if that fails, the user might not have root/admin
+     * privileges.
+     * 
+     * @return  true if the application has root/admin privileges.
+     */
+    public static boolean hasRootPrivileges()
+    {
+        // retrieve root preference node for the system
+        Preferences sysPrefs = Preferences.systemRoot();
+        
+        // retrieve standard error output stream
+        PrintStream errStream = System.err;
+        
+        // synchronize to avoid problems with other threads that access System.err
+        synchronized(errStream)
+        {
+            // override standard error output stream
+            System.setErr(new PrintStream(new OutputStream()
+            {    
+                @Override
+                public void write(int b) throws IOException
+                {
+                    // suppress error messages caused by this method
+                }
+            }));
+
+            try
+            {
+                // try to edit system preferences
+                sysPrefs.put("foo", "bar");     // SecurityException on Windows
+                sysPrefs.remove("foo");
+                sysPrefs.flush();               // BackingStoreException on Linux
+                
+                // no security exception thrown: I am root
+                return true;
+            }
+            catch (Exception exception)
+            {
+                // security exception thrown: I am not root
+                return false;
+            }
+            finally
+            {
+                // reset standard error output stream
+                System.setErr(System.err);
+            }
+        }
+    }
+
     /**
      * Developer harness test.
      * 
@@ -47,11 +104,13 @@ public final class JVMUtil
     {
         if (isJVM64Bit())
         {
-            System.out.println("You are running 64-bit JVM.");
+            System.out.println("You are running 64-bit JVM. Root privileges: "
+                                + hasRootPrivileges());
         }
         else
         {
-            System.out.println("You are running 32-bit JVM.");
+            System.out.println("You are running 32-bit JVM. Root privileges: "
+                                + hasRootPrivileges());
         }
     }
 }
